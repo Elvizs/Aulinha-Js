868c61cd46efcb6f93c4e1e736a509c1385ef6b5
Elvis-crazy <cassiano.elvis30@gmail.com>
2020-12-30 14:17:47 -0300

    Java/S

---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));*/
+
+
+
+
+
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+
+
+
+/*var count;
+    for(count = 0; count <= 5; count++){
+        alert(count);  
+    }*/
+
+
+
+/*var count = 0;
+while(count <= 5 ) {
+    console.log(count)
+    alert(count)
+    count++
+}*/
+
+
+
+
+/*var idade = prompt("Qual a sua idade?") 
+if (idade >= 18) {
+    alert("maior de idade")
+}else("menor de idade")
+    alert("menor de idade")*/
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelho"},{nome:"uva", cor:"roxa"}];
+    console.log(frutas);
+    alert(frutas[1].nome);*/
+
+
+
+/*var fruta = {nome:"maçã", cor:"vermelho"}
+console.log(fruta.nome);
+alert(fruta.cor)*/
+
+
+
+//var lista = ["maçã","pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.toString()[0]); //tira a prpriedade de array
+
+//var nome = "Elvis Cassiano";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japao é ruim";
+//alert(nome  + " tem " + idade);
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
